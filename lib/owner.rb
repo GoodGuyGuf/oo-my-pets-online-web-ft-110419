4643aaebd0a9f546bdfd742075a97a772b6d8869
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-26 03:43:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,7 +35,9 @@ attr_reader :name, :species
   end
 
   def buy_cat(name)
-    name.owner = self
+    bought_cat = Cat.new(name, self)
+    Cat.all << bought_cat.owner
+    cats
     end
 
   def buy_dog(name)
