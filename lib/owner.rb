b9407ad8fa9b12e98316d08b95bb5418ae5b559e
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-26 04:23:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,8 +51,8 @@ attr_reader :name, :species, :pets, :mood
   end
 
   def sell_pets
-    Cat.all.select{|cat| cat.owner == nil && cat.mood = "nervous"}
-    Dog.all.select{|dog| dog.owner == nil && dog.mood = "nervous"}
+    Cat.all.select{|cat| cat.owner != self && cat.mood = "nervous"}
+    Dog.all.select{|dog| dog.owner != self && dog.mood = "nervous"}
   end
 
 
